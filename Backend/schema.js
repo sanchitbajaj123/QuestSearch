cf3e0757737f232c5790ddd9cc9a0b6f820f4800
sanchit bajaj <sanchitbajaj2003@gmail.com>
2025-01-21 23:32:43 +0530

    first commit

---
+++ Backend/schema.js
@@ -0,0 +1,20 @@
+const mon=require('mongoose');
+
+mon.connect('mongodb://localhost:27017/assignment',{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log('Connected to database'))
+.catch((err)=>console.log('Error:',err));
+
+const schema=mon.Schema;
+
+const ProductSchema=new schema({
+type:String,
+anagramtype:String,
+blocks:[],
+siblingid:String,
+solution:String,
+title:String,
+},{timestamps:true});
+
+
+const Product=mon.model('Product',ProductSchema,'assignment');
+module.exports=Product;
